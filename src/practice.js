69e1867d9dd1373e833f6373f1e71616b27391de
Kellyaolmos@gmail.com <khachatur.h5@tumo.org>
2026-05-19 20:27:27 +0400

    asteroid

--- src/practice.js
+++ src/practice.js
@@ -16,3 +16,19 @@ console.log(issPosition.latitude);
 console.log(issPosition.longitude);
 console.log(issPosition);
 
+const asteroid =[
+   {name: "2024 ABI", diameter: 120,hazardous: false},
+     {name: "2024 CD2", diameter: 45,hazardous: true},
+       {name: "2024 EF3", diameter: 890,hazardous: false},
+         {name: "2024 GH4", diameter: 120,hazardous: true}
+   
+]
+ const names = asteroid.map(asteroid => asteroid.name);
+ const Hazardous = asteroid.map( asteroid =>asteroid.hazardous  === true);
+ const Diameter = asteroid.map( asteroid => asteroid.diameter);
+ console.log(names);
+ console.log(Hazardous);
+ console.log(Diameter);
+ 
+ 
+ 
