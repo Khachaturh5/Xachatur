90a768d5bffb7f48e76e9a410e0e9431da0f2e88
Kellyaolmos@gmail.com <khachatur.h5@tumo.org>
2026-05-19 19:54:46 +0400

    javascript und html love

---
+++ src/practice.js
@@ -0,0 +1,18 @@
+
+const name = "Xacho"
+const  craft = 'mars'
+
+function  nametexst(name,craft){
+   return name  +" is currently abrosd the "+craft 
+}
+console.log(nametexst(name,craft));
+
+const issPosition= {
+   latitude: "178",
+   longitude: "85"
+
+}
+console.log(issPosition.latitude);
+console.log(issPosition.longitude);
+console.log(issPosition);
+
